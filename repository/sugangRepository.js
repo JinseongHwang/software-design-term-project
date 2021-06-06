72611a5e748f82152781984e29fcd176832287de
JinseongHwang <eddy5360@naver.com>
2021-06-06 15:32:25 +0900

    refactor: 불필요한 주석 제거

--- repository/sugangRepository.js
+++ repository/sugangRepository.js
@@ -28,28 +28,3 @@ module.exports = {
   save,
   findContentsByStudentNumber,
 };
-
-// const sugangRepository = [];
-//
-// const save = async (studentNumber, subject, time) => {
-//   if (!sugangRepository[studentNumber]) {
-//     sugangRepository[studentNumber] = [{
-//       subject: subject,
-//       time: time,
-//     }];
-//   } else {
-//     await sugangRepository[studentNumber].push({
-//       subject: subject,
-//       time: time,
-//     });
-//   }
-// }
-//
-// const findContentByStudentNumber = async (studentNumber) => {
-//   return sugangRepository[studentNumber];
-// }
-//
-// module.exports = {
-//   save,
-//   findContentByStudentNumber,
-// };
